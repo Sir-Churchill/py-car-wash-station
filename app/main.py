7288c812bce2d10315bf7755b9f0be366d522721
Nik <your@mail.com>
2025-06-28 20:39:41 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,72 @@
+from typing import Iterable
+
 class Car:
-    # write your code here
-    pass
+    def __init__(self, comfort_class, clean_mark, brand):
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(self, distance_from_city_center, clean_power, average_rating, count_of_ratings):
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+
+    def serve_cars(self, car: Iterable[Car]) -> float:
+
+        res = []
+
+        for cars in car:
+
+            if cars.clean_mark < self.clean_power:
+                price = (cars.comfort_class * (
+                            self.clean_power - cars.clean_mark) * self.average_rating) / self.distance_from_city_center
+
+                res.append(round(price, 1))
+
+                cars.clean_mark = self.clean_power
+
+            else:
+                self.clean_power = cars.clean_mark
+            self.wash_single_car(cars)
+
+        total = sum(res)
+        return total
+
+
+    def wash_single_car(self, cars):
+        if cars.clean_mark == self.clean_power:
+            cars.clean_mark = self.clean_power
+        else:
+            self.clean_power = cars.clean_mark
+
+        return cars.clean_mark
+
+
+    def calculate_washing_price(self, car: Car) -> float:
+
+        res = []
+
+        if car.clean_mark < self.clean_power:
+                price = (car.comfort_class * (
+                        self.clean_power - car.clean_mark) * self.average_rating) / self.distance_from_city_center
+
+                res.append(round(price, 1))
+
+        total_sum = sum(res)
+
+        return total_sum
+
+
+    def rate_service(self, mark: int) -> float:
+        count = self.count_of_ratings
+
+        while count != count + 1:
+            self.average_rating = round((self.average_rating * self.count_of_ratings + mark) / (self.count_of_ratings + 1), 1)
+
+            self.count_of_ratings = self.count_of_ratings + 1
+            break
+        return self.average_rating
