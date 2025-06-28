41566368d36f7fb6bfb8c85cc07f8ce8595f7047
Nik <your@mail.com>
2025-06-28 23:33:05 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 from typing import Iterable
 
 class Car:
-    def __init__(self, comfort_class, clean_mark, brand):
+    def __init__(self, comfort_class:int, clean_mark:int, brand:str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center, clean_power, average_rating, count_of_ratings):
+    def __init__(self, distance_from_city_center:float, clean_power:int, average_rating:float, count_of_ratings:int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -36,15 +36,13 @@ class CarWashStation:
         total = sum(res)
         return total
 
-
-    def wash_single_car(self, cars):
+    def wash_single_car(self, cars: object) -> int:
         if cars.clean_mark == self.clean_power:
             cars.clean_mark = self.clean_power
         else:
             self.clean_power = cars.clean_mark
 
         return cars.clean_mark
-
 
     def calculate_washing_price(self, car: Car) -> float:
 
