fa8abdf7d9fc3bad72ec9bc8f202e162b991f3c3
Nik <your@mail.com>
2025-06-28 23:41:44 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,72 +2,68 @@ from typing import Iterable
 
 
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(self, comfort_class: int, clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int,
-                 average_rating: float, count_of_ratings: int) -> None:
+    def __init__(
+        self,
+        distance_from_city_center: float,
+        clean_power: int,
+        average_rating: float,
+        count_of_ratings: int
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
     def serve_cars(self, car: Iterable[Car]) -> float:
-
         res = []
 
         for cars in car:
-
             if cars.clean_mark < self.clean_power:
-                price = (cars.comfort_class * (self.clean_power - cars.clean_mark) *
-                         self.average_rating) / self.distance_from_city_center
+                price = (
+                    cars.comfort_class
+                    * (self.clean_power - cars.clean_mark)
+                    * self.average_rating
+                ) / self.distance_from_city_center
 
                 res.append(round(price, 1))
-
                 cars.clean_mark = self.clean_power
-
             else:
                 self.clean_power = cars.clean_mark
+
             self.wash_single_car(cars)
 
-        total = sum(res)
-        return total
+        return sum(res)
 
     def wash_single_car(self, cars: object) -> int:
         if cars.clean_mark == self.clean_power:
             cars.clean_mark = self.clean_power
         else:
             self.clean_power = cars.clean_mark
-
         return cars.clean_mark
 
     def calculate_washing_price(self, car: Car) -> float:
-
-        res = []
-
         if car.clean_mark < self.clean_power:
-            price = (car.comfort_class * (
-                    self.clean_power - car.clean_mark) *
-                    self.average_rating) / self.distance_from_city_center
-
-            res.append(round(price, 1))
-
-        total_sum = sum(res)
-
-        return total_sum
+            price = (
+                car.comfort_class
+                * (self.clean_power - car.clean_mark)
+                * self.average_rating
+            ) / self.distance_from_city_center
+            return round(price, 1)
+        return 0.0
 
     def rate_service(self, mark: int) -> float:
-        count = self.count_of_ratings
-
-        while count != count + 1:
-            self.average_rating = round((self.average_rating *
-                                         self.count_of_ratings + mark) /
-                                        (self.count_of_ratings + 1), 1)
-
-            self.count_of_ratings = self.count_of_ratings + 1
-            break
+        self.average_rating = round(
+            (self.average_rating * self.count_of_ratings + mark)
+            / (self.count_of_ratings + 1),
+            1
+        )
+        self.count_of_ratings += 1
         return self.average_rating
